6521f4a899697d9a37936f3175183e2d6db66af0
Cédric Royer <cedricroyerpro@gmail.com>
2024-08-07 12:54:39 +0200

    fetch recipes data and display in console

---
+++ scripts/script.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', () => {
+    fetch('data/recipes.JSON')
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok ' + response.statusText);
+            }
+            return response.json();
+        })
+        .then(data => {
+            console.log(data);
+        })
+        .catch(error => {
+            console.error('There has been a problem with your fetch operation:', error);
+        });
+});
