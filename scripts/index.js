ae4efdb8c128ffadfa4ea528877fca2449d1fbc0
Cedric Royer <cedricroyerpro@gmail.com>
2024-08-22 00:09:45 +0200

    add prevent default to search button

--- scripts/index.js
+++ scripts/index.js
@@ -93,5 +93,7 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     });
     
-    
+    document.getElementById('search-button').addEventListener('click', function(event) {
+        event.preventDefault(); // Empêche le comportement par défaut du bouton
+    });
 });
